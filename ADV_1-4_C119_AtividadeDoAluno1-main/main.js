f00cd1ebf8253abe2b51bda7e48f0631e18ccf40
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-10-24 19:45:34 -0300

    Update main.js

--- ADV_1-4_C119_AtividadeDoAluno1-main/main.js
+++ ADV_1-4_C119_AtividadeDoAluno1-main/main.js
@@ -1,6 +1,38 @@
 const divCam = document.getElementById('camera');
-
 const divSnap = document.getElementById('snapshot');
+
+//HOJEEEEEEEEEEEEEEEEE!
+const divControles = document.getElementById('controles');
+const divEmotion1 = document.getElementById('emotion1');
+const divEmoji1 = document.getElementById('emoji1');
+const divEmotion2 = document.getElementById('emotion2');
+const divEmoji2 = document.getElementById('emoji2');
+
+//colocar Modelo Ai (teatchablemachine);
+const URL = "https://teachablemachine.withgoogle.com/models/MsgOrdH27/";
+const modelURL = URL + "model.json";
+const metadataURL = URL + "metadata.json";
+const classifier = ml5.imageClassifier(modelURL, modelReady);
+
+const emocoes = {
+    "feliz": "&#128522;",
+    "triste": "&#128532;",
+    "irritado": "&#128548;"
+}
+
+function modelReady() {
+    console.log('Model Ready');
+    divControles.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -15,13 +47,35 @@ Webcam.set({
 //permissão
 Webcam.attach(divCam);
 
-Webcam.snap(dataURI => {
-    const ibagem = document.createElement("img");
-    ibagem.id = "captura";
-    ibagem.src = dataURI;
-    //colocar dentro do html
-    divSnap.innerHTML = "";
-    divSnap.appendChild(ibagem);
-    //speak("triste", "feliz");
-  
-});
+
+function begin(){
+    Webcam.snap(dataURI => {
+        const ibagem = document.createElement("img");
+        ibagem.id = "captura";
+        ibagem.src = dataURI;
+        //colocar dentro do html
+        divSnap.innerHTML = "";
+        divSnap.appendChild(ibagem);
+        //speak("triste", "feliz");
+        //Hojeeeeeeeeeeeeeeeeeeeee
+        divControles.style.display = "none";
+        classifier.classify(ibagem, gotResult)
+    
+      
+    });
+}
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        const resultado1 =  result[0].label;
+        const resultado2 =  result[1].label;
+        console.log(resultado1, resultado2);
+        speak(resultado1, resultado2);
+        preenchePrevisao(resultado1, divEmotion1, divEmoji1);
+        preenchePrevisao(resultado2, divEmotion2, divEmoji2);
+    }
+    divControles.style.display = "block";
+}
+
